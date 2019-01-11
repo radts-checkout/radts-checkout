67aa0ad3f6a0e72620cc070d522172c049e67fa3
yanghq23 <yanghh23@gmail.com>
2019-01-11 10:32:06 +0800

    添加工具类

--- radts-checkout/src/test/java/com/mediinfo/radts/RadtsCheckoutApplicationTests.java
+++ radts-checkout/src/test/java/com/mediinfo/radts/RadtsCheckoutApplicationTests.java
@@ -2,15 +2,23 @@ package com.mediinfo.radts;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RadtsCheckoutApplicationTests {
+    @Value("${server.port}")
+    public String port ;
 
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void readYmlTest() {
+        System.err.println(port);
+
+    }
 }
